2835d529c5e146fba0c950bca2c5f21acc22b12d
dennyzhang <denny@dennyzhang.com>
2017-10-18 14:44:17 -0500

    add more puzzles

---
+++ easy/fizz-buzz/test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/fizz-buzz/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-18 14:40:32>
+##-------------------------------------------------------------------
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ret = []
+        i = 0
+        while i<n:
+            if (i+1) % 15 == 0:
+                string = "FizzBuzz"
+            elif (i+1) % 3 == 0:
+                string = "Fizz"
+            elif (i+1) % 5 == 0:
+                string = "Buzz"
+            else:
+                string = str(i+1)
+            ret.append(string)
+            i += 1
+        return ret
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.fizzBuzz(4)
+    print s.fizzBuzz(15)
+## File: test.py ends
