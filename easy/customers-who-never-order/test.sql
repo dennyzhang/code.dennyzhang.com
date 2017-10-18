e1cfa72461876401b39d672a369384964f34d982
dennyzhang <denny@dennyzhang.com>
2017-10-18 18:55:01 -0500

    update code

---
+++ easy/customers-who-never-order/test.sql
@@ -0,0 +1,5 @@
+;; https://leetcode.com/problems/customers-who-never-order/description/
+
+select Customers.Name as Customers
+from Customers left join Orders on Customers.Id = Orders.CustomerId
+where Orders.CustomerId is Null;
