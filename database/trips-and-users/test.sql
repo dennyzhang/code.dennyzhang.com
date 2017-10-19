f5803c300d87cb073b283aa429d015740b2a1725
dennyzhang <denny@dennyzhang.com>
2017-10-19 12:05:50 -0500

    update sql

---
+++ database/trips-and-users/test.sql
@@ -0,0 +1,17 @@
+;; https://leetcode.com/problems/trips-and-users/description/
+
+select t1.Day as Day, round(COALESCE(t2.cancel_count, 0)/t1.total_count, 2) as 'Cancellation Rate' from
+(select Trips.Request_at as Day, count(1) as total_count
+from Trips inner join Users
+on Trips.Client_Id = Users.Users_Id
+where Users.Banned = 'No' and Users.Role = 'client'
+and Trips.Request_at >= '2013-10-01' and Trips.Request_at <= '2013-10-03'
+group by Trips.Request_at) t1 left join
+(select Trips.Request_at as Day,  count(1) as cancel_count
+from Trips inner join Users
+on Trips.Client_Id = Users.Users_Id
+where Users.Banned = 'No' and Users.Role = 'client'
+and Trips.Request_at >= '2013-10-01' and Trips.Request_at <= '2013-10-03'
+and Trips.Status != 'completed'
+group by Trips.Request_at) t2
+on t1.Day = t2. Day;
