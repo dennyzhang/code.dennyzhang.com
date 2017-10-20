83e7f72afc5d84e30c5d5af609ae5818bbe31a10
dennyzhang <denny@dennyzhang.com>
2017-10-20 10:31:15 -0500

    update doc

--- database/department-top-three-salaries/test.sql
+++ database/department-top-three-salaries/test.sql
@@ -1,13 +1,13 @@
 # https://leetcode.com/problems/department-top-three-salaries/description/
 
-{"headers": ["Department", "Employee", "Salary"], "values":
-[["IT", "Max", 90000],
-["IT", "Joe", 70000],
-["Sales", "Henry", 80000],
-["Sales", "Sam", 60000]]}
-
-select Department.Name as Department, Employee.Name as Employee, Employee.Salary
-from Employee inner join Department
-on Employee.DepartmentId = Department.Id
-group by Employee.DepartmentId
-having 
+select t4.Name as Department, t3.Name as Employee, t3.Salary
+from
+    (select t1.Id, t1.Name, t1.Salary, t1.DepartmentId, count(1) as rank
+    from Employee as t1 inner join Employee as t2
+    on t1.DepartmentId = t2.DepartmentId
+    where t1.Salary <= t2.Salary
+    group by t1.Id, t1.Name, t1.Salary, t1.DepartmentId) as t3
+    inner join Department as t4
+    on t3.DepartmentId = t4.Id
+where rank<=3
+order by t4.Name, t3.Salary desc
