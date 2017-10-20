9e3a2536117168b112e0d9fe74b1721ad8392050
dennyzhang <denny@dennyzhang.com>
2017-10-20 15:24:50 -0500

    update code

---
+++ algorithms/ugly-number/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/ugly-number/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-20 15:23:30>
+##-------------------------------------------------------------------
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+
+        for power in [2, 3, 5]:
+            while num % power == 0 and num != 0:
+                num = num / power
+
+        return num == 1
+        
+if __name__ == '__main__':
+    s = Solution()
+    print s.isUgly(1) #yes
+    print s.isUgly(14) #not
+    print s.isUgly(15) #yes
+    print s.isUgly(16) #yes
+## File: test.py ends
--- database/second-highest-salary/test.sql
+++ database/second-highest-salary/test.sql
@@ -1,3 +1,5 @@
 # https://leetcode.com/problems/second-highest-salary/description/
 
-select t.Salary as SecondHighestSalary from (select Salary from Employee order by Salary desc limit 2) as t limit 1 offset 1;
+select ifnull((
+       select Salary from Employee
+       group by Salary order by Salary desc limit 1,1), null) as SecondHighestSalary
