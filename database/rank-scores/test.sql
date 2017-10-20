a94a84096400ab4cdef34e047ca7e82126d5fe02
dennyzhang <denny@dennyzhang.com>
2017-10-19 20:49:58 -0500

    update code

--- database/rank-scores/test.sql
+++ database/rank-scores/test.sql
@@ -8,3 +8,16 @@ from Scores as t1 inner join
       group by Score order by Score desc) as t) As t2
 on t1.Score = t2.Score
 order by t1.Score desc
+
+# -- --8<-------------------------- separator -----------------------
+
+select ranking.Score as Score, ranking.Ranking as Rank
+    from Scores inner join
+      (select t1.Score as Score, count(1) as Ranking
+        from
+            (select distinct Score from Scores) as t1 inner join
+            (select distinct Score from Scores) as t2
+        where t1.Score <= t2.Score
+        group by t1.Score) as ranking
+    on Scores.Score = ranking.Score
+    order by ranking.Score desc
