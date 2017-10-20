ae55c110592db1f0e0a95bb2f64419626d4bae8f
dennyzhang <denny@dennyzhang.com>
2017-10-20 15:52:41 -0500

    update code

--- algorithms/add-binary/test.py
+++ algorithms/add-binary/test.py
@@ -10,7 +10,7 @@
 ##     https://leetcode.com/problems/add-binary/description/
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-20 15:35:38>
+## Updated: Time-stamp: <2017-10-20 15:51:24>
 ##-------------------------------------------------------------------
 class Solution(object):
     def addBinary(self, a, b):
@@ -19,6 +19,35 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        val1 = 0
+        for ch in a:
+            val1 = val1 << 1
+            if ch == '1':
+                val1 = val1 + 1
+            else:
+                if ch != '0':
+                    raise Exception("Invalid output")
+
+        val2 = 0
+        for ch in b:
+            val2 = val2 << 1
+            if ch == '1':
+                val2 = val2 + 1
+            else:
+                if ch != '0':
+                    raise Exception("Invalid output")
+
+        # print("val1: %s, val2: %s" % (val1, val2))
+        l = []
+        val = val1 + val2
+        if val == 0:
+            return "0"
+        else:
+            while val != 0:
+                l.append(str(val % 2))
+                val = val >> 1
+            l.reverse()
+            return "".join(l)
 
 if __name__ == '__main__':
     s = Solution()
