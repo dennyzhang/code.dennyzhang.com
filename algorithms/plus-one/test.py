574fe9f0dd4bb4a4478e27747eeb54dd157c19a6
dennyzhang <denny@dennyzhang.com>
2017-10-21 11:02:55 -0500

    update code

---
+++ algorithms/plus-one/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/plus-one/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-20 17:28:36>
+##-------------------------------------------------------------------
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.plusOne([1, 2, 0, 5])
+    print s.plusOne([0])
+    print s.plusOne([1, 2])
+## File: test.py ends
---
+++ algorithms/remove-duplicates-from-sorted-array/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-21 11:02:26>
+##-------------------------------------------------------------------
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        ret = 0
+        for i in range(0, len(nums)-1):
+            if nums[i] != nums[i+1]:
+                ret += 1
+                nums[ret] = nums[i+1]
+            i += 1
+        return (ret + 1)
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.removeDuplicates([1,1,2])
+    print s.removeDuplicates([1,2,1])
+    print s.removeDuplicates([2])
+    print s.removeDuplicates([])
+## File: test.py ends
