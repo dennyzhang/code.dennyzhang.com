dcc0aee4cfabe8accb040103c0d1a06db2dd0e19
dennyzhang <denny@dennyzhang.com>
2017-10-22 22:10:51 -0500

    update code

---
+++ algorithms/first-unique-character-in-a-string/test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/first-unique-character-in-a-string/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-22 22:10:13>
+##-------------------------------------------------------------------
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        NO_UNIQ = -1
+        ch_dict = {}
+        for i in range(0, len(s)):
+            ch = s[i]
+            if ch_dict.has_key(ch) is True:
+                ch_dict[ch] = NO_UNIQ
+            else:
+                ch_dict[ch] = i
+
+        if len(ch_dict.keys()) == 0:
+            return -1
+
+        min_index = -1
+        for ch in ch_dict:
+            if ch_dict[ch] == NO_UNIQ:
+                continue
+            if min_index == -1:
+                min_index = ch_dict[ch]
+            else:
+                if ch_dict[ch] < min_index:
+                    min_index = ch_dict[ch]
+        return min_index
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.firstUniqChar("leetcode")
+    print s.firstUniqChar("")
+    print s.firstUniqChar("loveleetcode")
+    print s.firstUniqChar("lovveleecodecd")
+## File: test.py ends
---
+++ algorithms/third-maximum-number/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/third-maximum-number/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-22 20:34:24>
+##-------------------------------------------------------------------
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_1st = None
+        max_2nd = None
+        max_3rd = None
+
+    def insertValue(self, list_3items, new_value):
+        
+if __name__ == '__main__':
+    s = Solution()
+    print s.thirdMax([1, 3])
+    print s.thirdMax([2, 2, 3, 1])
+## File: test.py ends
