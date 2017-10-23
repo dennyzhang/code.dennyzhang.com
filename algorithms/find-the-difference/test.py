2e134017ce396d0ba12165c9f44a28bb007dc688
dennyzhang <denny@dennyzhang.com>
2017-10-22 22:15:54 -0500

    update code

---
+++ algorithms/find-the-difference/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/find-the-difference/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-22 22:15:39>
+##-------------------------------------------------------------------
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+if __name__ == '__main__':
+    s = Solution()
+    print s.findTheDifference("abcd", "abcde")
+    print s.findTheDifference("abcd", "")
+    print s.findTheDifference("", "abcde")
+## File: test.py ends
