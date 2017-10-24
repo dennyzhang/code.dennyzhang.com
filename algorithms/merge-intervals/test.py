51f6d1ae3fd02324edbc275535e136efe9d81ce7
dennyzhang <denny@dennyzhang.com>
2017-10-23 22:28:56 -0500

    update code

--- algorithms/merge-intervals/test.py
+++ algorithms/merge-intervals/test.py
@@ -11,7 +11,7 @@
 ## Tags:
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-23 21:12:55>
+## Updated: Time-stamp: <2017-10-23 22:28:08>
 ##-------------------------------------------------------------------
 class Solution(object):
     def merge(self, intervals):
@@ -19,28 +19,10 @@ class Solution(object):
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
-        ## Basic Idea: Sort by first elements, then merge
-        ## Complexity: Time O(nlog(n)), Space O(1)
-        intervals = sorted(intervals, key=lambda x: x[0])
-
         ret = []
-
-        index = 0
-        for entry in intervals:
-            [start_val, end_val] = entry
-            if index == 0:
-                ret.append(entry)
-                index += 1
+        for entry in sorted(intervals, key=lambda entry: entry.start):
+            if ret and entry.start <= ret[-1].end:
+                ret[-1].end = max(ret[-1].end, entry.end)
             else:
-                [result_start_val, result_end_val] = ret[index-1]
-                if start_val == result_start_val or start_val <= result_end_val:
-                    ret[index-1][1] = max(end_val, result_end_val)
-                else:
-                    ret.append(entry)
-                    index += 1
+                ret.append(entry)
         return ret
-                    
-if __name__ == '__main__':
-    s = Solution()
-    print s.merge([[1,3],[8,10],[2,6],[15,18]])
-## File: test.py ends
