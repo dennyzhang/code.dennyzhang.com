d1e614b0e2446bd154acbfb4deb56d165de1ad9a
dennyzhang <denny@dennyzhang.com>
2017-10-24 17:21:43 -0500

    update code

---
+++ algorithms/combination-sum-iii/test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/combination-sum-iii/description/
+##    ,-----------
+##    | Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+##    | 
+##    | 
+##    | Example 1:
+##    | 
+##    | Input: k = 3, n = 7
+##    | 
+##    | Output:
+##    | 
+##    | [[1,2,4]]
+##    | 
+##    | Example 2:
+##    | 
+##    | Input: k = 3, n = 9
+##    | 
+##    | Output:
+##    | 
+##    | [[1,2,6], [1,3,5], [2,3,4]]
+##    `-----------
+##    
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-23 18:22:04>
+##-------------------------------------------------------------------
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.combinationSum3(3, 7)
+    print s.combinationSum3(3, 9)
+## File: test.py ends
--- algorithms/roman-to-integer/test.py
+++ algorithms/roman-to-integer/test.py
@@ -8,12 +8,10 @@
 ## Author : Denny <contact@dennyzhang.com>
 ## Description:
 ##     https://leetcode.com/problems/roman-to-integer/description/
-## Basic Idea:
-## Complexity:
 ## Tags:
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-23 18:22:04>
+## Updated: Time-stamp: <2017-10-24 17:21:30>
 ##-------------------------------------------------------------------
 class Solution(object):
     def romanToInt(self, s):
@@ -21,20 +19,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        ret = 0
-        roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
-        i = 0
-        j = 0
-        for ch in s:
-            ret += roman_dict[ch]
-        return ret
+        ## Basic Idea:
+        ## Complexity:
 
 if __name__ == '__main__':
     s = Solution()
-    print s.romanToInt("MCMXCVI") # 1996
+    print s.romanToInt("MCMXCVI")
     print s.romanToInt("III")
-    print s.romanToInt("VIII")    
-    print s.romanToInt("X")
-    print s.romanToInt("M")
-    print s.romanToInt("MM")
 ## File: test.py ends
