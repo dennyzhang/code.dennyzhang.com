f31d10184510958906bc249db3a47e52d612814a
dennyzhang <denny@dennyzhang.com>
2017-10-23 20:03:50 -0500

    update code

---
+++ algorithms/merge-two-sorted-lists/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/merge-two-sorted-lists/description/
+## Basic Idea:
+## Complexity:
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-23 18:41:01>
+##-------------------------------------------------------------------
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+## File: test.py ends
--- algorithms/reverse-linked-list/test.py
+++ algorithms/reverse-linked-list/test.py
@@ -13,7 +13,7 @@
 ## Tags:
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-23 18:22:04>
+## Updated: Time-stamp: <2017-10-23 20:02:09>
 ##-------------------------------------------------------------------
 class Solution(object):
     def reverseList(self, head):
@@ -21,13 +21,23 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        ## Basic Idea:
+        ##   1 -> 3 -> 4 -> 5
+        ##   p   q   r
+        ##   always insert to the head
+        if head is None or head.next is None:
+            return head
 
-if __name__ == '__main__':
-    s = Solution()
-    print s.romanToInt("MCMXCVI") # 1996
-    print s.romanToInt("III")
-    print s.romanToInt("VIII")    
-    print s.romanToInt("X")
-    print s.romanToInt("M")
-    print s.romanToInt("MM")
+        p = head
+        q = p.next
+        p.next = None
+
+        while True:
+            r = q.next
+            q.next = p
+            p = q
+            q = r
+            if q is None:
+                break
+        return p
 ## File: test.py ends
