afb6210495ebaa77fcaf43207318c26d896a6bf9
dennyzhang <denny@dennyzhang.com>
2017-10-25 16:39:39 -0500

    update code

--- algorithms/isomorphic-strings/test.py
+++ algorithms/isomorphic-strings/test.py
@@ -9,6 +9,23 @@
 ## Description:
 ##     https://leetcode.com/problems/isomorphic-strings/description/
 ## Tags: #redo, #amusing
+## ,-----------
+## | Given two strings s and t, determine if they are isomorphic.
+## | 
+## | Two strings are isomorphic if the characters in s can be replaced to get t.
+## | 
+## | All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+## | 
+## | For example,
+## | Given "egg", "add", return true.
+## | 
+## | Given "foo", "bar", return false.
+## | 
+## | Given "paper", "title", return true.
+## | 
+## | Note:
+## | You may assume both s and t have the same length.
+## `-----------
 ## --
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-25 16:38:15>
