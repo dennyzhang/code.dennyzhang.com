31a99229e33d0517e12059c180ff034363b7c4ef
dennyzhang <denny@dennyzhang.com>
2017-10-25 09:48:07 -0500

    update code

---
+++ algorithms/decode-ways/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/decode-ways/description/
+##    ,-----------
+##    | A message containing letters from A-Z is being encoded to numbers using the following mapping:
+##    | 
+##    | 'A' -> 1
+##    | 'B' -> 2
+##    | ...
+##    | 'Z' -> 26
+##    | Given an encoded message containing digits, determine the total number of ways to decode it.
+##    | 
+##    | For example,
+##    | Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+##    | 
+##    | The number of ways decoding "12" is 2.
+##    `-----------
+##
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-24 23:50:33>
+##-------------------------------------------------------------------
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
+## File: test.py ends
--- algorithms/remove-nth-node-from-end-of-list/test.py
+++ algorithms/remove-nth-node-from-end-of-list/test.py
@@ -22,7 +22,7 @@
 ##    `-----------
 ##    
 ##    
-## Tags:
+## Tags: #amusing
 ## --
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-24 17:21:30>
@@ -33,38 +33,80 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
+class MinStack(object):
+
+    ## Idea: List with elements of (value, min_value)
+    ## 
+    ## -2 0 -3  4
+    ## linked list:
+    ##     (-2, -2), (0, -2), (-3, -3), (4, -3)
+    ##
+    ## length: element count
+    ##
+    ##
+    ## Complexity:  Time: O(1), Space: O(n)
+
+    def __init__(self):
         """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
+        initialize your data structure here.
         """
-        ## Basic Idea:
-        ##       1 -> 2  -> 3 -> 4 -> 5, n=2
-        ##                  p   q   r
-        ## Complexity: Time O(n), Space O(1)
-        length = 0
-        p = head
-        while (p is not None):
-            length += 1
-            p = p.next
+        self.length = 0
+        self.min_value = None
+        self.values = []
+        
 
-        if length < n:
-            raise Exception("Link only have %d nodes. Can't delete %d th node" % (length, n))
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if self.length == 0:
+            element = (x, x)
+            self.min_value = x
+        else:
+            if x > self.min_value:
+                element = (x, self.min_value)
+            else:
+                self.min_value = x
+                element = (x, x)
+        self.values.append(element)
+        self.length += 1
 
-        # remove the head
-        if n == length:
-            return head.next
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self.length == 0:
+            return
 
-        p = head
-        # move the pointer
-        for i in range(0, length - n - 1):
-            p = p.next
+        if self.length == 1:
+            self.min_value = None
+        else:
+            (x, previous_min_value) = self.values[-2]
+            self.min_value = previous_min_value
 
-        # remove the node
-        q = p.next
-        r = q.next
-        p.next = r
-        return head
-## File: test.py ends
+        self.values.pop()
+        self.length -= 1
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.values) == 0:
+            return None
+        else:
+            (x, _min_value) = self.values[-1]
+            return x
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_value
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
