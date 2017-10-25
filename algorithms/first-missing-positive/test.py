5b4090f32ed9bae369bf22a895958df0c122a0c0
dennyzhang <denny@dennyzhang.com>
2017-10-25 17:30:51 -0500

    update code

---
+++ algorithms/first-missing-positive/test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/first-missing-positive/description/
+## ,-----------
+## | Given an unsorted integer array, find the first missing positive integer.
+## | 
+## | For example,
+## | Given [1,2,0] return 3,
+## | and [3,4,-1,1] return 2.
+## | 
+## | Your algorithm should run in O(n) time and uses constant space.
+## `-----------
+## 
+## Tags: #amusing, #redo
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 17:22:00>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+        ##   [1, 2, 0]
+        ##   [3, 4, 0, 1]
+        ##   [3, 5, 0, 1]        
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
+## File: test.py ends
