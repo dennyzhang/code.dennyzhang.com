a760beac324e2474eeec018a6042b5393021b9ad
dennyzhang <denny@dennyzhang.com>
2017-10-25 14:04:31 -0500

    update code

---
+++ algorithms/majority-element/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/majority-element/description/
+##    ,-----------
+##    | Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+##    | 
+##    | You may assume that the array is non-empty and the majority element always exist in the array.
+##    `-----------
+##
+## Tags: amusing
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 14:04:07>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
+## File: test.py ends
