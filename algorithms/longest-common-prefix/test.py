40dd64a33ce27643a0638964a4a4ef4a4609d773
dennyzhang <denny@dennyzhang.com>
2017-10-25 18:03:09 -0500

    update code

--- algorithms/longest-common-prefix/test.py
+++ algorithms/longest-common-prefix/test.py
@@ -11,18 +11,36 @@
 ## Tags: #redo
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-25 17:59:43>
+## Updated: Time-stamp: <2017-10-25 18:03:00>
 ##-------------------------------------------------------------------
 class Solution(object):
-    def romanToInt(self, s):
+    def longestCommonPrefix(self, strs):
         """
-        :type s: str
-        :rtype: int
+        :type strs: List[str]
+        :rtype: str
         """
-        ## Basic Idea:
-        ## Complexity:
+        ## Idea: Compare first two, then get a common prefix. Then compare it with 3rd, then the following
+        ## Complexity: Time O(n*n), Space O(1)
+        str_count = len(strs)
+        if str_count == 0:
+            return ""
+        if str_count == 1:
+            return strs[0]
 
-if __name__ == '__main__':
-    s = Solution()
-    # print s.romanToInt("MCMXCVI")
-## File: test.py ends
+        common_prefix = None
+        common_length = None
+        for i in range(0, str_count):
+            string = strs[i]
+            if common_prefix is None:
+                common_prefix = string
+                common_length = len(common_prefix)
+            else:
+                common_length = min(len(string), common_length)
+                for j in range(0, common_length):
+                    if string[j] != common_prefix[j]:
+                        common_length = j
+                        break
+        if common_length == 0:
+            return ""
+        else:
+            return common_prefix[0:common_length]
