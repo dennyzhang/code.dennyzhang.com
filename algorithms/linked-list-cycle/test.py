272a5ac8479949b211c37527eee038cefa7b1681
dennyzhang <denny@dennyzhang.com>
2017-10-25 11:03:41 -0500

    update code

--- algorithms/linked-list-cycle/test.py
+++ algorithms/linked-list-cycle/test.py
@@ -20,16 +20,35 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-25 10:47:58>
 ##-------------------------------------------------------------------
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ## Basic Idea:
-        ## Complexity:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-if __name__ == '__main__':
-    s = Solution()
-    # print s.romanToInt("MCMXCVI")
-## File: test.py ends
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        ## Idea: 2 pointers. Each time, p1 move 1 step, p2 move 2 step. 
+        ##         If it has a cycle(not only it's a circle), p1 and p2 will meet. 
+        ##         Otherwise p1 and p2 won't.
+        ## Complexity: Time O(n), Space O(1)
+        ##
+        ##     n1    ->   n2    -> n3
+        ##     p1
+        ##                p2
+        if head is None or head.next is None:
+            return False
+        p1 = head
+        p2 = head.next
+        while (p1 is not None) and (p2 is not None) and (p2 != p1):
+            p1 = p1.next
+            if (p2.next is None) or (p2.next is None):
+                p2 = None
+            else:
+                p2 = p2.next.next
+    
+        return  (p1 is not None) and (p2 is not None) and (p2 == p1)
