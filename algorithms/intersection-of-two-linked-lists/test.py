d7f8d57889f1f98ed7bf8559e8ce07d2c253fda8
dennyzhang <denny@dennyzhang.com>
2017-10-26 18:54:21 -0500

    update code

--- algorithms/intersection-of-two-linked-lists/test.py
+++ algorithms/intersection-of-two-linked-lists/test.py
@@ -35,15 +35,50 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-25 18:27:45>
 ##-------------------------------------------------------------------
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ## Basic Idea:
-        ## Complexity:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-if __name__ == '__main__':
-    s = Solution()
-    # print s.romanToInt("MCMXCVI")
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        ## Idea: get the count of two links
+        ##       Skip the longer link with a proper offset.
+        ##       Then compare two links node by node
+        ## Complexity: Time O(n), Space O(1)
+        p = headA
+        length1 = 0
+        while (p is not None):
+            length1 += 1
+            p = p.next
+
+        q = headB
+        length2 = 0
+        while (q is not None):
+            length2 += 1
+            q = q.next
+
+        p = headA
+        q = headB
+        if length1 > length2:
+            offset = length1 - length2
+            while offset != 0:
+                p = p.next
+                offset -= 1
+        else:
+            if length1 < length2:
+                offset = length2 - length1
+                while offset != 0:
+                    q = q.next
+                    offset -= 1
+        # check items
+        while (p is not None) and (p != q):
+            p = p.next
+            q = q.next
+        return p        
+        
