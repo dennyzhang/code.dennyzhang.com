6e71718e4b46524c337a77b5c0ab2704e3cd18c5
dennyzhang <denny@dennyzhang.com>
2017-10-25 23:23:43 -0500

    update code

---
+++ algorithms/super-pow/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/super-pow/description/
+##    ,-----------
+##    | Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+##    | 
+##    | Example1:
+##    | 
+##    | a = 2
+##    | b = [3]
+##    | 
+##    | Result: 8
+##    | Example2:
+##    | 
+##    | a = 2
+##    | b = [1,0]
+##    | 
+##    | Result: 1024
+##    `-----------
+##
+## Tags: amusing, redo
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
