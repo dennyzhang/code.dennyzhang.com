ff92e8fef74da308d8abcfc97139e773de4d94b2
dennyzhang <denny@dennyzhang.com>
2017-10-25 21:18:49 -0500

    update code

--- algorithms/single-number/test.py
+++ algorithms/single-number/test.py
@@ -8,6 +8,13 @@
 ## Author : Denny <contact@dennyzhang.com>
 ## Description:
 ##      https://leetcode.com/problems/single-number/description/
+## ,-----------
+## | Given an array of integers, every element appears twice except for one. Find that single one.
+## | 
+## | Note:
+## | Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+## `-----------
+##
 ## Basic Idea:
 ## Complexity:
 ## Tags:
