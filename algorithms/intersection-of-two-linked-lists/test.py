025ccb75888d7791ccbde2a7b3d1814973e1f8fd
dennyzhang <denny@dennyzhang.com>
2017-10-26 18:46:42 -0500

    update code

---
+++ algorithms/intersection-of-two-linked-lists/test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+##    ,-----------
+##    | Write a program to find the node at which the intersection of two singly linked lists begins.
+##    | 
+##    | 
+##    | For example, the following two linked lists:
+##    | 
+##    | A:          a1 → a2
+##    |                    ↘
+##    |                      c1 → c2 → c3
+##    |                    ↗            
+##    | B:     b1 → b2 → b3
+##    | begin to intersect at node c1.
+##    | 
+##    | 
+##    | Notes:
+##    | 
+##    | If the two linked lists have no intersection at all, return null.
+##    | The linked lists must retain their original structure after the function returns.
+##    | You may assume there are no cycles anywhere in the entire linked structure.
+##    | Your code should preferably run in O(n) time and use only O(1) memory.
+##    `-----------
+##
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
