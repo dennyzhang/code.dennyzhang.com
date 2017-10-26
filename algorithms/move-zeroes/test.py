baaccc2aa904a9299de2f28f8e9085b7ca55783a
dennyzhang <denny@dennyzhang.com>
2017-10-25 23:34:38 -0500

    update code

---
+++ algorithms/move-zeroes/test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/move-zeroes/description/
+##    ,-----------
+##    | Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+##    | 
+##    | For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+##    | 
+##    | Note:
+##    | You must do this in-place without making a copy of the array.
+##    | Minimize the total number of operations.
+##    `-----------
+##
+## Tags: #redo
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
