2918e5bbec0c3160f318a2cce05e0d315b06cdf9
dennyzhang <denny@dennyzhang.com>
2017-10-27 18:35:29 -0500

    update code

---
+++ algorithms/sum-of-square-numbers/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/sum-of-square-numbers/description/
+##    ,-----------
+##    | Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+##    | 
+##    | Example 1:
+##    | Input: 5
+##    | Output: True
+##    | Explanation: 1 * 1 + 2 * 2 = 5
+##    | Example 2:
+##    | Input: 3
+##    | Output: False
+##    `-----------
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
