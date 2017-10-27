82275b1f7c61d643643d888ebc14b9bddcb8c115
dennyzhang <denny@dennyzhang.com>
2017-10-27 18:51:49 -0500

    update code

---
+++ algorithms/sqrtx/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/sqrtx/description/
+##    ,-----------
+##    | Implement int sqrt(int x).
+##    | 
+##    | Compute and return the square root of x.
+##    `-----------
+##
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-27 18:48:08>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
---
+++ algorithms/sqrtxksum-of-square-numbers/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/roman-to-integer/description/
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
