5fdf40a8a2ee4d7c43cb1e030bde7110c3c67e2d
dennyzhang <denny@dennyzhang.com>
2017-10-27 18:44:55 -0500

    update code

--- algorithms/sum-of-square-numbers/test.py
+++ algorithms/sum-of-square-numbers/test.py
@@ -19,20 +19,42 @@
 ##    | Input: 3
 ##    | Output: False
 ##    `-----------
-## Tags:
+## Tags: #redo
 ## --
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-25 18:27:45>
 ##-------------------------------------------------------------------
 class Solution(object):
-    def romanToInt(self, s):
+    def judgeSquareSum(self, c):
         """
-        :type s: str
-        :rtype: int
+        :type c: int
+        :rtype: bool
         """
-        ## Basic Idea:
-        ## Complexity:
+        ## Idea: a = 1, 2, 3, ...
+        ## Complexity: Time O(log2(n)*log2(n)), Space O(1)
+        import math
+        a = 0
+        while (a*a <= c):
+            v = c-a*a
+            # Python check whether it's square
+            if int(v**.5)**2 == v:
+                return True
+            a += 1
+        return False
 
-if __name__ == '__main__':
-    s = Solution()
-    # print s.romanToInt("MCMXCVI")
+    def judgeSquareSum1(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        ## Idea: a = 1, 2, 3, ...
+        ## Complexity: Time O(log2(n)*log2(n)), Space O(1)
+        import math
+        a = 0
+        while (a*a <= c):
+            v = c-a*a
+            k = int(math.sqrt(v))
+            if k*k == v:
+                return True
+            a += 1
+        return False
