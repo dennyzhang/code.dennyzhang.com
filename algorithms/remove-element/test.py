ed5a510850415ddad8e97445f11d5c656fbf3b2e
dennyzhang <denny@dennyzhang.com>
2017-10-28 08:52:54 -0500

    update code

--- algorithms/remove-element/test.py
+++ algorithms/remove-element/test.py
@@ -8,9 +8,22 @@
 ## Author : Denny <contact@dennyzhang.com>
 ## Description:
 ##     https://leetcode.com/problems/remove-element/description/
+##    ,-----------
+##    | Given an array and a value, remove all instances of that value in place and return the new length.
+##    | 
+##    | Do not allocate extra space for another array, you must do this in place with constant memory.
+##    | 
+##    | The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+##    | 
+##    | Example:
+##    | Given input array nums = [3,2,2,3], val = 3
+##    | 
+##    | Your function should return length = 2, with the first two elements of nums being 2.
+##    `-----------
+##
 ## Basic Idea:
 ## Complexity:
-## Tags:
+## Tags: #redo
 ## --
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-23 18:22:04>
