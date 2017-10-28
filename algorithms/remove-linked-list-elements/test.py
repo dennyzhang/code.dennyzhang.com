48a70c32c10cc9c64979f9006d4b7ffe470af868
dennyzhang <denny@dennyzhang.com>
2017-10-28 08:49:47 -0500

    update code

---
+++ algorithms/remove-linked-list-elements/test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description:
+##     https://leetcode.com/problems/remove-linked-list-elements/description/
+##    ,-----------
+##    | Remove all elements from a linked list of integers that have value val.
+##    | 
+##    | Example
+##    | Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+##    | Return: 1 --> 2 --> 3 --> 4 --> 5
+##    | 
+##    `-----------
+##
+## Tags:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-25 18:27:45>
+##-------------------------------------------------------------------
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        ## Idea:
+        ## Complexity:
+        ## Sample Data:
+        ##  1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+        ##        p     q     r
+        # When head is None, or keep removing the head
+        while (head is not None) and (head.val == val):
+            head = head.next
+        if head is None:
+            return head
+        p = head
+        q = p.next
+        while q is not None:
+            if q.val == val:
+                # remove the node
+                r = q.next
+                p.next = r
+                q = r
+            else:
+                p = q
+                q = q.next
+        return head
