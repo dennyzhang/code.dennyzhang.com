7acc27c45fb75a1d18adac4a36e7908be2b486db
dennyzhang <denny@dennyzhang.com>
2017-10-30 08:42:35 -0500

    update code

---
+++ algorithms/triangle/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags: #amusing
+## Description:
+##     https://leetcode.com/problems/triangle/description/
+##    ,-----------
+##    | Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+##    | 
+##    | For example, given the following triangle
+##    | [
+##    |      [2],
+##    |     [3,4],
+##    |    [6,5,7],
+##    |   [4,1,8,3]
+##    | ]
+##    | The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+##    | 
+##    | Note:
+##    | Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+##    | 
+##    `-----------
+##
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:14>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
---
+++ algorithms/valid-triangle-number/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags: #amusing
+## Description:
+##     https://leetcode.com/problems/valid-triangle-number/description/
+##    ,-----------
+##    | Given an array consists of non-negative integers, your task is to count the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+##    | 
+##    | Example 1:
+##    | Input: [2,2,3,4]
+##    | Output: 3
+##    | Explanation:
+##    | Valid combinations are: 
+##    | 2,3,4 (using the first 2)
+##    | 2,3,4 (using the second 2)
+##    | 2,2,3
+##    | Note:
+##    | The length of the given array won't exceed 1000.
+##    | The integers in the given array are in the range of [0, 1000].
+##    `-----------
+##
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:14>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
