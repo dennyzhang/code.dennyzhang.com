36d53f9b4d6335ee720f743a44a03727f4b782be
dennyzhang <denny@dennyzhang.com>
2017-10-29 21:19:24 -0500

    update code

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ More website:
 **How many pointers you use? For what purpose?**
 - Remove Duplicates from Sorted List: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
 - Remove Remove Duplicates from Sorted List II: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
-- Reverse elements from i to j
+- Reverse elements from i to j: https://leetcode.com/problems/reverse-linked-list-ii/description/
 
 ## Integer
 - swap two values
---
+++ algorithms/symmetric-tree/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags:
+## Description:
+##     https://leetcode.com/problems/roman-to-integer/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:14>
+##-------------------------------------------------------------------
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ## Basic Idea:
+        ## Complexity:
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.romanToInt("MCMXCVI")
