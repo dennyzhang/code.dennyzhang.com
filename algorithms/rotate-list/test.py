6c2e5c074145340ade23cc223b22972f5a027212
dennyzhang <denny@dennyzhang.com>
2017-10-30 14:34:37 -0500

    update code

--- algorithms/rotate-list/test.py
+++ algorithms/rotate-list/test.py
@@ -9,6 +9,14 @@
 ## Tags:
 ## Description:
 ##     https://leetcode.com/problems/rotate-list/description/
+##    ,-----------
+##    | Given a list, rotate the list to the right by k places, where k is non-negative.
+##    | 
+##    | For example:
+##    | Given 1->2->3->4->5->NULL and k = 2,
+##    | return 4->5->1->2->3->NULL.
+##    `-----------
+##
 ## Basic Idea:
 ##      1->2->3->4->5->NULL
 ##      1->2->3->    4->5->NULL
