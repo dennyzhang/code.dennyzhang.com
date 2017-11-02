bfa3641816a1e530a36f9736897713db52c7a746
dennyzhang <denny@dennyzhang.com>
2017-11-01 23:23:16 -0500

    update code

---
+++ leetcode_python/algorithms/multiply-strings/test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags: #redo
+## Description:
+##     https://leetcode.com/problems/multiply-strings/description/
+##    ,-----------
+##    | Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+##    | 
+##    | Note:
+##    | 
+##    | The length of both num1 and num2 is < 110.
+##    | Both num1 and num2 contains only digits 0-9.
+##    | Both num1 and num2 does not contain any leading zero.
+##    | You must not use any built-in BigInteger library or convert the inputs to integer directly.
+##    `-----------
+##
+##
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-30 15:41:52>
+##-------------------------------------------------------------------
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        ## Idea: x^n = x^(n/2) * x^(n/2) * x^(n%2)
+        ## Complexity: Time O(log(n)), Space O(1)
+        ## Sample Data:
+        ##   pow(5, 3) = 5*5*5
+        ##   pow(5, -3) = ?
+        ##   pow(-5, 3) = (-5)*(-5)*(-5)
+        ##   pow(5.1, 3) = 5.1*5.1*5.1
+        ##   x^n = x^(n/2) * x^(n/2) * x^(n%2)
+        ##   pow(x, -n) = 1/pow(x, n)
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1/self.myPow(x, -n)
+        if n == 1:
+            return x
+        if(n>=2):
+            val = self.myPow(x, n/2)
+            return val*val*self.myPow(x, n%2)
--- leetcode_python/algorithms/perfect-number/test.py
+++ leetcode_python/algorithms/perfect-number/test.py
@@ -9,6 +9,17 @@
 ## Tags:
 ## Description:
 ##     https://leetcode.com/problems/perfect-number/description/
+##    ,-----------
+##    | We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+##    | 
+##    | Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+##    | Example:
+##    | Input: 28
+##    | Output: True
+##    | Explanation: 28 = 1 + 2 + 4 + 7 + 14
+##    | Note: The input number n will not exceed 100,000,000. (1e8)
+##    `-----------
+##
 ## Basic Idea:
 ## Complexity:
 ## --
@@ -21,9 +32,17 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-
-if __name__ == '__main__':
-    s = Solution()
-    print s.checkPerfectNumber(28)
-    print s.checkPerfectNumber(1)
-## File: test.py ends
+        ## Idea: sqrt(num)
+        ## Complexity:
+        ## Sample Data:
+        ##    1 2 7
+        if num <= 1:
+            return False
+        import math
+        sum = 1
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                sum += i
+                if i != num/i:
+                    sum += num/i
+        return sum == num
