dfebc5ee51437e62ba6c36131f072763f3ce0bb8
dennyzhang <denny@dennyzhang.com>
2017-11-02 09:13:12 -0500

    update code

--- leetcode_python/algorithms/triangle/test.py
+++ leetcode_python/algorithms/triangle/test.py
@@ -6,7 +6,7 @@
 ##
 ## File: test.py
 ## Author : Denny <contact@dennyzhang.com>
-## Tags: #amusing
+## Tags: #amusing, #redo
 ## Description:
 ##     https://leetcode.com/problems/triangle/description/
 ##    ,-----------
@@ -32,6 +32,40 @@
 ##-------------------------------------------------------------------
 class Solution(object):
     def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        ## Idea: From bottom to top.
+        ##       a + b + c ... + e
+        ##       To get the mininum sum, e always have 2 possibilites.
+        ##       And we need to choose the smaller one
+        ##       last_mininum_list: mininum total from current layer to the bottom
+        ##       Notice: use space of existing array
+        ##
+        ## Complexity: Time O(n*n), Space O(1)
+        ## Sample Data
+        ##         -1
+        ##        2  3
+        ##      1  -1  -3
+        ##
+        ##          1
+        ##        2   3
+        if len(triangle) == 0:
+            return None
+        if len(triangle) == 1:
+            return triangle[0][0]
+    
+        for j in xrange(len(triangle[-1])-1):
+            triangle[-1][j] = min(triangle[-1][j], triangle[-1][j+1])
+
+        for i in range(len(triangle)-2, 0, -1):
+            for j in xrange(len(triangle[i])-1):
+                triangle[i][j] = min(triangle[i][j]+triangle[i+1][j], \
+                                     triangle[i][j+1]+triangle[i+1][j+1])
+        return triangle[0][0]+triangle[1][0]
+
+    def minimumTotal_v1(self, triangle):
         """
         :type triangle: List[List[int]]
         :rtype: int
