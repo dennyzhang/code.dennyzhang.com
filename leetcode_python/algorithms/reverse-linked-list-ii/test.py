47bcdaa2354335d654d7db7b6bcdd031e391726c
dennyzhang <denny@dennyzhang.com>
2017-11-02 11:11:07 -0500

    update code

--- leetcode_python/algorithms/reverse-linked-list-ii/test.py
+++ leetcode_python/algorithms/reverse-linked-list-ii/test.py
@@ -26,6 +26,12 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-28 21:01:14>
 ##-------------------------------------------------------------------
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def reverseBetween(self, head, m, n):
         """
@@ -34,8 +40,44 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        ## Basic Idea:
-        ##   1->2->3->4->5->NULL, m = 2 and n = 4,
-        ##      2->3->4
-        ##   p  q     r  s
-## File: test.py ends
+        ## Idea: Insert to a given pointer for (n-m) times.
+        ##       1. Change in the head, thus we need a dummy head node
+        ##       2. Need to track get previous node of sub list
+        ##       3. Need to track the head of original sub list
+        ##       4. How many times each loop shall compute
+        ## Complexity: Time O(n), Space O(1)
+        ## Sample Data
+        ##      1->2->3->4->5->6->NULL, m = 2 and n = 5
+        ##         .  .  .  .
+        ##      p     q  r
+        ##
+        ##      1->2->3->4->5->NULL, m = 1 and n = 3
+        ##      .  .  .
+        ##      p
+        ##
+        ##      3->5
+        ##     dummy -> 3 -> 5
+        ##       p
+        ##              q     r
+        ##     dummy -> 1 -> 2 -> 3
+        ##       p      q    r
+        # Nothing changed
+        if m == n:
+            return head
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        p = dummyNode
+        # p: points to the previous node of the sublist head
+        for i in xrange(m-1):
+            p = p.next
+        # r points to current node
+        # q points to the original first node of the sublist
+        # No need to reverse the first node
+        q = p.next
+        r = q.next
+        for i in xrange(n-m):
+            q.next = r.next
+            r.next = p.next
+            p.next = r
+            r = q.next
+        return dummyNode.next
