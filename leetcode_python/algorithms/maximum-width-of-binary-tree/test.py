76c593d7b14a493c18678a33e0777cf4e2ff9fc5
dennyzhang <denny@dennyzhang.com>
2017-11-06 17:06:06 -0600

    update code

--- leetcode_python/algorithms/maximum-width-of-binary-tree/test.py
+++ leetcode_python/algorithms/maximum-width-of-binary-tree/test.py
@@ -69,28 +69,3 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-11-06 09:42:17>
 ##-------------------------------------------------------------------
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        ## Idea: x^n = x^(n/2) * x^(n/2) * x^(n%2)
-        ## Complexity: Time O(log(n)), Space O(1)
-        ## Sample Data:
-        ##   pow(5, 3) = 5*5*5
-        ##   pow(5, -3) = ?
-        ##   pow(-5, 3) = (-5)*(-5)*(-5)
-        ##   pow(5.1, 3) = 5.1*5.1*5.1
-        ##   x^n = x^(n/2) * x^(n/2) * x^(n%2)
-        ##   pow(x, -n) = 1/pow(x, n)
-        if n == 0:
-            return 1
-        if n < 0:
-            return 1/self.myPow(x, -n)
-        if n == 1:
-            return x
-        if(n>=2):
-            val = self.myPow(x, n/2)
-            return val*val*self.myPow(x, n%2)
