31752c461eccec4a7446522330af2223f0230267
dennyzhang <denny@dennyzhang.com>
2017-11-05 20:57:36 -0600

    update code

--- leetcode_python/algorithms/symmetric-tree/test.py
+++ leetcode_python/algorithms/symmetric-tree/test.py
@@ -33,28 +33,70 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-11-03 10:14:57>
 ##-------------------------------------------------------------------
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
-    def myPow(self, x, n):
+    def isSymmetric(self, root):
         """
-        :type x: float
-        :type n: int
-        :rtype: float
+        :type root: TreeNode
+        :rtype: bool
         """
-        ## Idea: x^n = x^(n/2) * x^(n/2) * x^(n%2)
-        ## Complexity: Time O(log(n)), Space O(1)
-        ## Sample Data:
-        ##   pow(5, 3) = 5*5*5
-        ##   pow(5, -3) = ?
-        ##   pow(-5, 3) = (-5)*(-5)*(-5)
-        ##   pow(5.1, 3) = 5.1*5.1*5.1
-        ##   x^n = x^(n/2) * x^(n/2) * x^(n%2)
-        ##   pow(x, -n) = 1/pow(x, n)
-        if n == 0:
-            return 1
-        if n < 0:
-            return 1/self.myPow(x, -n)
-        if n == 1:
-            return x
-        if(n>=2):
-            val = self.myPow(x, n/2)
-            return val*val*self.myPow(x, n%2)
+        ## Idea: BFS. pop 2 elements each time.
+        ## Complexity
+        if root is None:
+            return True
+        if (root.left is None) is not (root.right is None):
+            return False
+        if root.left is None:
+            return True
+
+        queue = []
+        queue.append(root.left)
+        queue.append(root.right)
+        while len(queue) != 0:
+            element1 = queue[-1]
+            del queue[-1]
+            element2 = queue[-1]
+            del queue[-1]
+            if element1.val != element2.val:
+                return False
+
+            if (element1.left is None) is not (element2.right is None):
+                return False
+
+            if (element1.left is not None):
+                queue.append(element1.left)
+                queue.append(element2.right)
+
+            if (element1.right is None) is not (element2.left is None):
+                return False
+
+            if (element1.right is not None):
+                queue.append(element1.right)
+                queue.append(element2.left)
+        return True
+
+    def isSymmetric_v1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root:
+            return self._isSymmetric(root.left, root.right)
+        else:
+            return True
+
+    def _isSymmetric(self, node1, node2):
+        if node1 is None or node2 is None:
+            return (node1 is None) is (node2 is None)
+
+        if node1.val != node2.val:
+            return False
+        else:
+            return self._isSymmetric(node1.left, node2.right) and \
+                    self._isSymmetric(node1.right, node2.left)
