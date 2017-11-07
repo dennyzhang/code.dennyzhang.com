04a71a701301e79d44dab83b736219ebf0544a8b
dennyzhang <denny@dennyzhang.com>
2017-11-06 18:30:28 -0600

    update code

--- leetcode_python/algorithms/palindrome-linked-list/test.py
+++ leetcode_python/algorithms/palindrome-linked-list/test.py
@@ -23,23 +23,58 @@
 ## Created : <2017-10-16>
 ## Updated: Time-stamp: <2017-10-28 21:01:16>
 ##-------------------------------------------------------------------
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
-    def checkPerfectNumber(self, num):
+    def isPalindrome(self, head):
         """
-        :type num: int
+        :type head: ListNode
         :rtype: bool
         """
-        ## Idea: sqrt(num)
+        ## Idea: Can I destroy existing link?
+        ##       1. Insert the last half into the head: 
+        ##          From: a->b->c->b->a
+        ##          To:   a->b->a->b->c
+        ##       Then two pointers to check
         ## Complexity:
-        ## Sample Data:
-        ##    1 2 7
-        if num <= 1:
-            return False
-        import math
-        sum = 1
-        for i in range(2, int(math.sqrt(num))+1):
-            if num % i == 0:
-                sum += i
-                if i != num/i:
-                    sum += num/i
-        return sum == num
+        if head is None or head.next is None:
+            return True
+        # find length
+        length = 0
+        p = head
+        while p:
+            length += 1
+            p = p.next
+
+        dummy = ListNode(None)
+        dummy.next = head
+
+        p = dummy
+        for i in xrange((length+1)/2):
+            p = p.next
+        q = p
+        p = dummy
+        # move sublist to the head
+        ## dummy -> a -> b
+        ##   p      q    r
+        for i in xrange(length/2):
+            r = q.next
+            q.next = r.next
+            r.next = p.next
+            p.next = r
+        # move 2 pointers
+        p = dummy
+        for i in xrange(length/2):
+            p = p.next
+        q = p
+        p = dummy
+        for i in xrange(length/2):
+            if p.next.val != q.next.val:
+                return False
+            p = p.next
+            q = q.next
+        return True
