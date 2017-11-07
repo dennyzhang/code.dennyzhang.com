25a59c934baa1b287dca422cddcb2b372db12419
dennyzhang <denny@dennyzhang.com>
2017-11-07 10:06:26 -0600

    Update code

---
+++ leetcode_python/algorithms/house-robber/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags:
+## Description:
+##     https://leetcode.com/problems/house-robber/description/
+##    ,-----------
+##    | You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+##    | 
+##    | Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+##    | 
+##    `-----------
+##
+## Basic Idea:
+## Complexity:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:16>
+##-------------------------------------------------------------------
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        ## Idea: sqrt(num)
+        ## Complexity:
+        ## Sample Data:
+        ##    1 2 7
+        if num <= 1:
+            return False
+        import math
+        sum = 1
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                sum += i
+                if i != num/i:
+                    sum += num/i
+        return sum == num
