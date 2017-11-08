12c656f6c7b88f5c09e2d675741d53b633b9209b
dennyzhang <denny@dennyzhang.com>
2017-11-08 11:28:01 -0600

    update code

--- leetcode_python/algorithms/sum-of-left-leaves/test.py
+++ leetcode_python/algorithms/sum-of-left-leaves/test.py
@@ -13,12 +13,53 @@
 ## Complexity:
 ## --
 ## Created : <2017-10-16>
-## Updated: Time-stamp: <2017-10-28 21:01:13>
+## Updated: Time-stamp: <2017-11-08 09:40:15>
+##-------------------------------------------------------------------
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+##     https://leetcode.com/problems/sum-of-left-leaves/description/
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-20 16:55:53>
 ##-------------------------------------------------------------------
 class Solution(object):
+    ## Idea: BFS. (element, is_left)
+    ##       DFS
+    ## Complexity: 
     def sumOfLeftLeaves(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-## File: test.py ends
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 0
+
+        return self._sumOfLeftLeaves(root, False)
+
+    def _sumOfLeftLeaves(self, root, is_left):
+        if root is None:
+            return 0
+
+        if root.left is None and root.right is None:
+            if is_left:
+                return root.val
+            else:
+                return 0
+
+        sum_value = 0
+        if root.left:
+            sum_value += self._sumOfLeftLeaves(root.left, True)
+
+        if root.right:
+            sum_value += self._sumOfLeftLeaves(root.right, False)
+
+        return sum_value
