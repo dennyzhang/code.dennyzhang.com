4e8a146e37082a482bda80abe276ce2266ce19d1
dennyzhang <denny@dennyzhang.com>
2017-11-11 16:16:46 -0600

    update code

--- leetcode_python/algorithms/bitwise-and-of-numbers-range/test.py
+++ leetcode_python/algorithms/bitwise-and-of-numbers-range/test.py
@@ -23,22 +23,45 @@
 ## Updated: Time-stamp: <2017-10-28 21:01:16>
 ##-------------------------------------------------------------------
 class Solution(object):
-    def checkPerfectNumber(self, num):
+    def rangeBitwiseAnd(self, m, n):
         """
-        :type num: int
-        :rtype: bool
+        :type m: int
+        :type n: int
+        :rtype: int
         """
-        ## Idea: sqrt(num)
+        ## Idea: 2^k is in the middle of [m, n], the result is 0
         ## Complexity:
-        ## Sample Data:
-        ##    1 2 7
-        if num <= 1:
-            return False
-        import math
-        sum = 1
-        for i in range(2, int(math.sqrt(num))+1):
-            if num % i == 0:
-                sum += i
-                if i != num/i:
-                    sum += num/i
-        return sum == num
+        if m < 0:
+            return 0
+        if m == 0:
+            return 0
+        if m == n:
+            return m
+
+        m_log2 = 0
+        pow_m = 1
+        while m > pow_m:
+            pow_m = pow_m*2
+            m_log2 += 1
+
+        n_log2 = 0
+        pow_n = 1
+        while n > pow_n:
+            pow_n = pow_n*2
+            n_log2 += 1
+
+        if n == pow_n:
+            return 0
+
+        if m == pow_m:
+            if n >= pow_m*2:
+                return 0
+            else:
+                return m
+        if n_log2 > m_log2:
+            return 0
+
+        # print("m: %d, pow_m: %d" % (m, pow_m))
+        res = pow_m/2
+        res += self.rangeBitwiseAnd(m-res, n-res)
+        return res
