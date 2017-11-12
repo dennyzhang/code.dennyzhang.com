c4d67e4b363c431646810296064b1f8847398160
dennyzhang <denny@dennyzhang.com>
2017-11-11 19:41:23 -0600

    update code

---
+++ leetcode_python/algorithms/container-with-most-water/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags:
+## Description:
+##     https://leetcode.com/problems/container-with-most-water/description/
+##    ,-----------
+##    | Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+##    | 
+##    | Note: You may not slant the container and n is at least 2.
+##    `-----------
+##
+##
+## Basic Idea:
+## Complexity:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:16>
+##-------------------------------------------------------------------
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        ## Idea: sqrt(num)
+        ## Complexity:
+        ## Sample Data:
+        ##    1 2 7
+        if num <= 1:
+            return False
+        import math
+        sum = 1
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                sum += i
+                if i != num/i:
+                    sum += num/i
+        return sum == num
---
+++ leetcode_python/algorithms/reconstruct-original-digits-from-english/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <contact@dennyzhang.com>
+## Tags: #todobrain, #redo
+## Description:
+##     https://leetcode.com/problems/reconstruct-original-digits-from-english/description/
+##    ,-----------
+##    | Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+##    | 
+##    | Note:
+##    | Input contains only lowercase English letters.
+##    | Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+##    | Input length is less than 50,000.
+##    | Example 1:
+##    | Input: "owoztneoer"
+##    | 
+##    | Output: "012"
+##    | Example 2:
+##    | Input: "fviefuro"
+##    | 
+##    | Output: "45"
+##    `-----------
+##
+##
+## Basic Idea:
+## Complexity:
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-28 21:01:16>
+##-------------------------------------------------------------------
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        ## Idea: sqrt(num)
+        ## Complexity:
+        ## Sample Data:
+        ##    1 2 7
+        if num <= 1:
+            return False
+        import math
+        sum = 1
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                sum += i
+                if i != num/i:
+                    sum += num/i
+        return sum == num
