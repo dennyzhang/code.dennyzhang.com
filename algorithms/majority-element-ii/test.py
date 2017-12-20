d03764323d7fef07f81df29ac3a38c4d32ce1836
DennyZhang <contact@dennyzhang.com>
2017-12-20 11:42:34 +0800

    add more quiz

---
+++ algorithms/majority-element-ii/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 brain.dennyzhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <http://brain.dennyzhang.com/contact>
+## Tags: #redo, #todobrain
+## Description:
+##     https://leetcode.com/problems/majority-element-ii/description/
+##    ,-----------
+##    | Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+##    `-----------
+##    
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-12-20 11:42:24>
+##-------------------------------------------------------------------
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        ## Basic Idea:
+        ## Complexity: Time O(), Space O()
+        ## Assumptions:
+        ## Sample Data:
+        ## barfoothefoobarman -> bar foo the foo bar man
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.findSubstring("barfoothefoobarman")
+## File: test.py ends
