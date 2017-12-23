54be0644ecdb9a963095d164e4930625cf18da99
DennyZhang <contact@dennyzhang.com>
2017-12-23 15:36:43 +0800

    add more quiz

---
+++ algorithms/remove-k-digits/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 brain.dennyzhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <http://brain.dennyzhang.com/contact>
+## Tags:
+## Description:
+##     https://leetcode.com/problems/remove-k-digits/description/
+##    ,-----------
+##    | Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+##    | 
+##    | Note:
+##    | The length of num is less than 10002 and will be ≥ k.
+##    | The given num does not contain any leading zero.
+##    | Example 1:
+##    | 
+##    | Input: num = "1432219", k = 3
+##    | Output: "1219"
+##    | Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+##    | Example 2:
+##    | 
+##    | Input: num = "10200", k = 1
+##    | Output: "200"
+##    | Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+##    | Example 3:
+##    | 
+##    | Input: num = "10", k = 2
+##    | Output: "0"
+##    | Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+##    `-----------
+##    
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-24 17:21:30>
+##-------------------------------------------------------------------
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        ## Basic Idea:
+        ## Complexity: Time O(), Space O()
+        ## Assumptions:
+        ## Sample Data:
+        ## barfoothefoobarman -> bar foo the foo bar man
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.findSubstring("barfoothefoobarman")
+## File: test.py ends
