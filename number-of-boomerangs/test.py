638e16000ea342659444e35356d8fb34cf3cce54
DennyZhang <contact@dennyzhang.com>
2018-01-09 10:16:43 -0600

    add code

---
+++ number-of-boomerangs/test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+##-------------------------------------------------------------------
+## @copyright 2017 brain.dennyzhang.com
+## Licensed under MIT
+##   https://www.dennyzhang.com/wp-content/mit_license.txt
+##
+## File: test.py
+## Author : Denny <http://brain.dennyzhang.com/contact>
+## Tags: #redo, #list
+## Description:
+##     https://leetcode.com/problems/number-of-boomerangs/description/
+##    ,-----------
+##    | Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+##    | 
+##    | Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
+##    | 
+##    | Example:
+##    | Input:
+##    | [[0,0],[1,0],[2,0]]
+##    | 
+##    | Output:
+##    | 2
+##    | 
+##    | Explanation:
+##    | The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+##    `-----------
+##    
+## --
+## Created : <2017-10-16>
+## Updated: Time-stamp: <2017-10-24 17:21:30>
+##-------------------------------------------------------------------
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        ## Basic Idea:
+        ## Complexity: Time O(), Space O()
+        ## Assumptions:
+        ## Sample Data:
+        ## barfoothefoobarman -> bar foo the foo bar man
+
+if __name__ == '__main__':
+    s = Solution()
+    # print s.findSubstring("barfoothefoobarman")
+## File: test.py ends
