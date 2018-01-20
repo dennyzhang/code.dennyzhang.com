24ebfc1a8967c620df579cbe4814026a98ce1891
DennyZhang <contact@dennyzhang.com>
2018-01-19 21:29:38 -0600

    update code

---
+++ crontab.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /Users/DennyZhang/Dropbox/private_data/project/devops_consultant/consultant_code/github/leetcode/challenges-leetcode-interesting
+
+./automate.sh refresh_link
+## File: hello ends
