174939e7021c4ea9ef275bf9ee34ceccc4950a6f
DennyZhang <contact@dennyzhang.com>
2018-02-12 00:09:19 -0600

    remove folder

--- misc/database/classes-more-than-5-students/test.sql
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/classes-more-than-5-students/description/
-
-select class
-from (select class, count(distinct student) as num from courses group by class) as t
-where t.num >= 5;
-
-SELECT class
-FROM courses
-GROUP BY class HAVING COUNT(DISTINCT student)>=5;
