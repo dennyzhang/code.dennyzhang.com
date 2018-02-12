8573cbd9b2f85d61c11ae507fcb9faaa00ffd2bc
DennyZhang <contact@dennyzhang.com>
2018-02-11 22:05:02 -0600

    update sql problems

--- misc/database/rising-temperature/test.sql
+++
@@ -1,6 +0,0 @@
-# https://leetcode.com/problems/rising-temperature/description/
-
-select t1.Id
-from Weather as t1 join Weather as t2
-on DATE_ADD(t2.Date, INTERVAL 1 DAY) = t1.Date
-where t1.Temperature > t2.Temperature;
--- misc/database/second-highest-salary/test.sql
+++
@@ -1,5 +0,0 @@
-# https://leetcode.com/problems/second-highest-salary/description/
-
-select ifnull((
-       select Salary from Employee
-       group by Salary order by Salary desc limit 1,1), null) as SecondHighestSalary
