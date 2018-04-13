b4ff267654eef70a35d619d553da6dc26c8639e7
DennyZhang <contact@dennyzhang.com>
2018-04-13 18:42:24 -0500

    rename url

--- automate.sh
+++ automate.sh
@@ -13,9 +13,9 @@ function refresh_link() {
     echo "refresh link"
     for f in $(ls -1t */README.org); do
         dirname=$(basename $(dirname $f))
-        if ! grep "Blog link: https:\/\/brain.dennyzhang.com.*$dirname" $f 1>/dev/null 2>&1; then
+        if ! grep "Blog link: https:\/\/code.dennyzhang.com.*$dirname" $f 1>/dev/null 2>&1; then
             echo "Update blog url for $f"
-            sed -ie "s/Blog link: https:\/\/brain.dennyzhang.com\/.*/Blog link: https:\/\/brain.dennyzhang.com\/$dirname/g" $f
+            sed -ie "s/Blog link: https:\/\/code.dennyzhang.com\/.*/Blog link: https:\/\/code.dennyzhang.com\/$dirname/g" $f
             rm -rf $dirname/README.orge
         fi
 
